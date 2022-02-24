b8c33b74d9317a1a1f0a15e27c2138aeb0353bda
Felipe Fraxe <felipe_fraxe@hotmail.com>
2022-02-24 15:57:13 -0400

    Changed merge function to interactive method

--- sort/merge_sort/merge.c
+++ sort/merge_sort/merge.c
@@ -11,21 +11,21 @@
 #include "merge.h"
 
 // Define Array's size
-#define SIZE 95000
+#define SIZE 1000000
 
 void swap(int *elem1, int *elem2);
-void generate_array(int *array, int index, int size);
-void print_array(int *array, int index, int size);
+void generate_array(int *array, int size);
+void print_array(int *array, int size);
 
 int main(void)
 {
   srand(time(0));
   int array[SIZE];
-  generate_array(array, 0, SIZE);
+  generate_array(array, SIZE);
   clock_t t = clock();
   merge_sort(array, 0, SIZE - 1);
   t = clock() - t;
-  print_array(array, 0, SIZE);
+  print_array(array, SIZE);
   printf("This method toomain_index %lf seconds to sort\n", (double) t / CLOCKS_PER_SEC);
   return EXIT_SUCCESS;
 }
@@ -45,47 +45,27 @@ void merge(int *array, int left, int mid, int right)
 {
   int size1 = (mid + 1) - left;
   int size2 = right - mid;
-  int array1[size1];
-  int array2[size2];
-  fill_array(array, left, array1, 0, size1);
-  fill_array(array, mid + 1, array2, 0, size2);
-  get_in_order(array, left, array1, 0, size1, array2, 0, size2);
-}
+  int array1[size1], array2[size2];
 
-void fill_array(int *main_array, int main_index, int *array, int index, int size)
-{
-  if(index < size)
-  {
-    array[index] = main_array[main_index];
-    fill_array(main_array, main_index + 1, array, index + 1, size);
-  }
-}
+  for(int i = 0, k = left; i < size1; i++, k++)
+    array1[i] = array[k];
 
-void get_in_order(int *main_array, int main_index, int *array1, int index1, int size1, int *array2, int index2, int size2)
-{
-  if(index1 < size1 && index2 < size2)
+  for(int j = 0, k = mid + 1; j < size2; j++, k++)
+    array2[j] = array[k];
+
+  int i = 0, j = 0, k = left;
+  while(i < size1 && j < size2)
   {
-    if(array1[index1] < array2[index2])
-    {
-      main_array[main_index] = array1[index1];
-      get_in_order(main_array, main_index + 1, array1, index1 + 1, size1, array2, index2, size2);
-    }
+    if(array1[i] < array2[j])
+      array[k++] = array1[i++];
     else
-    {
-      main_array[main_index] = array2[index2];
-      get_in_order(main_array, main_index + 1, array1, index1, size1, array2, index2 + 1, size2);
-    }
+      array[k++] = array2[j++];
   }
-  else if(index1 < size1)
-  {
-    main_array[main_index] = array1[index1];
-    get_in_order(main_array, main_index + 1, array1, index1 + 1, size1, array2, index2, size2);
-  }
-  else if(index2 < size2)
-  {
-    main_array[main_index] = array2[index2];
-    get_in_order(main_array, main_index + 1, array1, index1, size1, array2, index2 + 1, size2);
-  }
+
+  while(i < size1)
+    array[k++] = array1[i++];
+  while(j < size2)
+    array[k++] = array2[j++];
 }
 
 void swap(int *elem1, int *elem2)
@@ -95,22 +75,15 @@ void swap(int *elem1, int *elem2)
   *elem2 = tmp;
 }
 
-void generate_array(int *array, int index, int size)
+void generate_array(int *array, int size)
 {
-  if(index < size)
-  {
-    array[index] = rand() / (RAND_MAX / SIZE + 1);
-    generate_array(array, index + 1, size);
-  }
+  for(int i = 0; i < size; i++)
+    array[i] = rand() / (RAND_MAX / size + 1);
 }
 
-void print_array(int *array, int index, int size)
+void print_array(int *array, int size)
 {
-  if(index < size)
-  {
-    printf("%d ", array[index]);
-    print_array(array, index + 1, size);
-  }
-  else
-    printf("\n");
+  for(int i = 0; i < size; i++)
+    printf("%d ", array[i]);
+  printf("\n");
 }
