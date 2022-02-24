fdf1808df63333049ea6cc4193cbbc1c601f665c
Felipe Fraxe <felipe_fraxe@hotmail.com>
2022-02-24 16:17:23 -0400

    Changed bubble sort to interactive

--- sort/bubble_sort/bubble.h
+++ sort/bubble_sort/bubble.h
@@ -2,7 +2,6 @@
 #define BUBBLE_H
 
 // Prototypes for Bubble Sort
-void bubble_sort(int *array, int index, int size);
-void bubble(int *array, int index, int size);
+void bubble_sort(int *array, int size);
 
 #endif
