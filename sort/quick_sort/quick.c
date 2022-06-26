00c5b25678facde20e577ff8b745ecd8f2b39d59
Felipe Fraxe <felipe_fraxe@hotmail.com>
2022-06-26 03:15:26 -0400

    quick sort now accepts custom compare functions

--- sort/quick_sort/quick.c
+++ sort/quick_sort/quick.c
@@ -17,38 +17,48 @@
 void swap(void *elem1, void *elem2, size_t size);
 void generate_array(int *array, int size);
 void print_array(int *array, int size);
+int intcmp(void *ptr1, void *ptr2); 
 
 int main(void)
 {
   srand(time(0));
   int array[SIZE];
   generate_array(array, SIZE);
+
   clock_t t = clock();
-  quick_sort(array, 0, SIZE - 1);
+  quick_sort(array, 0, SIZE - 1, intcmp);
   t = clock() - t;
+
   print_array(array, SIZE);
   printf("This method took %lf seconds to sort\n", (double) t / CLOCKS_PER_SEC);
   return EXIT_SUCCESS;
 }
 
-void quick_sort(int *array, int left, int right)
+int intcmp(void *ptr1, void *ptr2)
+{
+  int *num1 = ptr1;
+  int *num2 = ptr2;
+  return *num1 - *num2;
+}
+
+void quick_sort(int *array, int left, int right, int (*cmp)(void *, void *))
 {
   if(left < right)
   {
     int pivot = (array[left] + array[(left + right) / 2] + array[right]) / 3;
-    int frontier = partition(array, left, right, pivot);
-    quick_sort(array, left, frontier - 1);
-    quick_sort(array, frontier, right);
+    int frontier = partition(array, left, right, pivot, cmp);
+    quick_sort(array, left, frontier - 1, cmp);
+    quick_sort(array, frontier, right, cmp);
   }
 }
 
-int partition(int *array, int left, int right, int pivot)
+int partition(int *array, int left, int right, int pivot, int (*cmp)(void *, void *))
 {
   while(left < right)
   {
-    while(array[left] <= pivot && left < right)
+    while(cmp(&array[left], &pivot) <= 0 && left < right)
       left++;
-    while(array[right] > pivot && left < right)
+    while(cmp(&array[right], &pivot) > 0 && left < right)
       right--;
     if(left < right)
     {
--- sort/quick_sort/quick.h
+++ sort/quick_sort/quick.h
@@ -2,7 +2,7 @@
 #define QUICK_H
 
 // Quick Sort prototypes
-void quick_sort(int *array, int left, int right);
-int partition(int *array, int left, int right, int pivot);
+void quick_sort(int *array, int left, int right, int (*cmp)(void *, void *));
+int partition(int *array, int left, int right, int pivot, int (*cmp)(void *, void *));
 
 #endif
