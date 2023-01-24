6312edd01acdb1585c63c7fbf25a3f87a4251db7
Felipe Fraxe <felipe_fraxe@hotmail.com>
2023-01-24 19:15:51 -0400

    Reafctored some code to make more readeble. Also, changed pivot to median of three strategy

--- sort/quick_sort/quick.c
+++ sort/quick_sort/quick.c
@@ -17,89 +17,97 @@ void generate_array(int *array, int size);
 void print_array(int *array, int size);
 int stringcmp(const void *string1, const void *string2);
 int intcmp(const void *ptr1, const void *ptr2);
+int is_sorted(int arr[], int length);
 
 int main(void)
 {
-  srand(time(NULL));
-  int array[SIZE];
-  generate_array(array, SIZE);
+	srand(time(NULL));
+  	int array[SIZE];
+  	generate_array(array, SIZE);
 
-  clock_t t = clock();
-  quick_sort(array, sizeof(int), 0, SIZE - 1, intcmp);
-  t = clock() - t;
-  print_array(array, SIZE);
-  printf("This method took %lf seconds to sort\n", (double) t / CLOCKS_PER_SEC);
+	clock_t t = clock();
+  	quick_sort(array, sizeof(int), 0, SIZE - 1, intcmp);
+  	t = clock() - t;
 
-  return 0;
+	is_sorted(array, SIZE) ? printf("SORTED\n") : printf("NOT SORTED\n");
+
+  	printf("This method took %lf seconds to sort\n", (double) t / CLOCKS_PER_SEC);
+  	return 0;
 }
 
 int intcmp(const void *num1, const void *num2)
 {
-  return *(int *)num1 - *(int *)num2;
+	return *(int *)num1 - *(int *)num2;
 }
 
 void quick_sort(void *array, size_t size, int left, int right, int (*cmp)(const void *, const void *))
 {
-  if(left >= right)
-    return;
-  int i = left, j = right;
-  partition(array, size, &i, &j, cmp);
-  if(left < j)
-    quick_sort(array, size, left, j, cmp);
-  if(i < right)
-    quick_sort(array, size, i, right, cmp);
+  	if(left >= right)
+    	return;
+	int p = partition(array, size, left, right, cmp);
+	quick_sort(array, size, left, p-1, cmp);
+	quick_sort(array, size, p, right, cmp);
 }
 
-void partition(void *array, size_t size, int *left, int *right, int (*cmp)(const void *, const void *))
+int partition(void *array, size_t size, int left, int right, int (*cmp)(const void *, const void *))
 {
-  int mid = *left + ((*right - *left) / 2);
-  void *pivot = array + (mid * size);
+	int mid = left + (right - left) / 2;
+	if(cmp((array + (left * size)), (array + (mid * size))) > 0)
+    	swap((array + (left * size)), (array + (mid * size)), size);
 
-  if(cmp((array + (*left * size)), pivot) > 0)
-    swap((array + (*left * size)), pivot, size);
+  	if(cmp((array + (right * size)), (array + (mid * size))) > 0)
+    	swap((array + (right * size)), (array + (mid * size)), size);
+	else if(cmp((array + (left * size)), (array + (right * size))) > 0)
+		swap((array + (right * size)), (array + (left * size)), size);
 
-  if(cmp((array + (*right * size)), pivot) < 0)
-    swap((array + (*right * size)), pivot, size);
+	void *pivot = array + (right * size);
 
-  if(cmp((array + (*left * size)), pivot) > 0)
-    swap((array + (*left * size)), pivot, size);
-
-  pivot = array + (*right * size);
-  while(*left <= *right)
-  {
-    while(cmp((array + (*left * size)), pivot) < 0)
-      (*left)++;
-    while(cmp((array + (*right * size)), pivot) > 0)
-      (*right)--;
-    if(*left <= *right)
-    {
-      swap((array + (*left * size)), (array + (*right * size)), size);
-      (*left)++;
-      (*right)--;
-    }
-  }
+  	while(left <= right)
+  	{
+    	while(cmp((array + (left * size)), pivot) < 0)
+      		left++;
+    	while(cmp((array + (right * size)), pivot) > 0)
+      		right--;
+    	if(left <= right)
+    	{
+      		swap((array + (left * size)), (array + (right * size)), size);
+      		left++;
+      		right--;
+    	}
+  	}
+	return left;
 }
 
 void swap(void *elem1, void *elem2, size_t size)
 {
-  char *ptr1 = (char *) elem1, *ptr2 = (char *) elem2;
-  while(size-- > 0)
-  {
-    char tmp = *ptr1;
-    *ptr1++ = *ptr2;
-    *ptr2++ = tmp;
-  }
+  	char *ptr1 = (char *) elem1, *ptr2 = (char *) elem2;
+  	while(size-- > 0)
+  	{
+    	char tmp = *ptr1;
+    	*ptr1++ = *ptr2;
+    	*ptr2++ = tmp;
+  	}
 }
 
 void generate_array(int *array, int size)
 {
-  for(int i = 0; i < size; i++)
-    array[i] = rand() / (RAND_MAX / size + 1);
+  	for(int i = 0; i < size; i++)
+    	array[i] = rand() / (RAND_MAX / size + 1);
 }
 
 void print_array(int *array, int size)
 {
-  for(int i = 0; i < size; i++)
-    printf("%d ", array[i]);
-  printf("\n");
+  	for(int i = 0; i < size; i++)
+    	printf("%d ", array[i]);
+  	printf("\n");
+}
+
+int is_sorted(int arr[], int length)
+{
+	for(int i = 0; i < length - 1; i++)
+	{
+		if(arr[i] > arr[i+1])
+			return 0;
+	}
+	return 1;
 }
--- sort/quick_sort/quick.h
+++ sort/quick_sort/quick.h
@@ -3,6 +3,6 @@
 
 // Quick Sort prototypes
 void quick_sort(void *array, size_t size, int left, int right, int (*cmp)(const void *, const void *));
-void partition(void *array, size_t size, int *left, int *right, int (*cmp)(const void *, const void *));
+int partition(void *array, size_t size, int left, int right, int (*cmp)(const void *, const void *));
 
 #endif
