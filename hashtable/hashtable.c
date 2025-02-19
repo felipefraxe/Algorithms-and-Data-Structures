3e8a0a3b56d6227aabd2f13938bf71fd0f8a1676
Felipe Fraxe <felipe_fraxe@hotmail.com>
2025-02-19 12:23:21 -0400

    feat: Hashtable completly refactored

--- hashtable/hashtable.c
+++ hashtable/hashtable.c
@@ -1,136 +1,205 @@
+#include <limits.h>
+#include <stdlib.h>
+
 #include "hashtable.h"
 
-size_t wcshash(wchar_t *key, size_t length)
-{
-    size_t index = 5381;
-    wint_t c;
-    while ((c = *key++) != L'\0')
-        index = (index * 33) + c;
-    return index % length;
-}
+static bool is_prime(size_t num) {
+    if (num < 2)
+        return false;
 
+    if (num % 2 == 0)
+        return num == 2;
 
-size_t strhash(char *key, size_t length)
-{
-    size_t index = 5381;
-    int c;
-    while ((c = *key++) != '\0')
-        index = (index * 33) + c;
-    return index % length;
-}
-
-
-HTable_Data *htable_create_data(char *key, char *value)
-{
-    HTable_Data *data = malloc(sizeof(HTable_Data));
-    if(data == NULL)
+    for (size_t i = 3; i * i <= num; i += 2)
     {
-        fprintf(stderr, "Mem allocation error\n");
-        exit(EXIT_FAILURE);
+        if (num % i == 0)
+            return false;
     }
 
-    data->key = malloc((strlen(key) + 1));
-    data->value = malloc((strlen(value) + 1));
-    if(data->key == NULL || data->value == NULL)
-    {
-        fprintf(stderr, "Mem allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-    strcpy(data->key, key);
-    strcpy(data->value, value);
-
-    return data;
+    return true;
 }
 
-
-void htable_deallocate_data(HTable_Data *data)
+static size_t next_prime(size_t num)
 {
-    free(data->key);
-    free(data->value);
-    free(data);
+    while (!is_prime(num))
+        num++;
+    return num;
 }
 
-
-HTable_Node *htable_create_node(HTable_Data *data)
+static htable_node_t *node_alloc(uint64_t key, int value)
 {
-    HTable_Node *node = malloc(sizeof(HTable_Node));
-    if(node == NULL)
-    {
-        fprintf(stderr, "Mem allocation error\n");
-        exit(EXIT_FAILURE);
-    }
+    htable_node_t *node = malloc(sizeof(htable_node_t));
 
-    node->data = data;
-    node->next = NULL;
+    node->key = key;
+    node->value = value;
+    node->next = nullptr;
+
     return node;
 }
 
-
-HTable_Node **htable_create(size_t length)
+uint64_t murmurhash3_64(const void *key, size_t len, uint32_t seed)
 {
-    HTable_Node **hashtable = malloc(sizeof(HTable_Node) * length);
-    if(hashtable == NULL)
+    const uint8_t *data = (const uint8_t *) key;
+    const int nblocks = len / 8;
+    uint64_t h1 = seed;
+
+    const uint64_t c1 = 0x87c37b91114253d5ULL;
+    const uint64_t c2 = 0x4cf5ad432745937fULL;
+
+    const uint64_t *blocks = (const uint64_t *)(data);
+    for (int i = 0; i < nblocks; i++)
     {
-        fprintf(stderr, "Mem allocation error\n");
-        exit(EXIT_FAILURE);
+        uint64_t k1 = blocks[i];
+
+        k1 *= c1;
+        k1 = (k1 << 31) | (k1 >> 33);
+        k1 *= c2;
+        h1 ^= k1;
+
+        h1 = (h1 << 27) | (h1 >> 37);
+        h1 = h1 * 5 + 0x52dce729;
     }
-    return hashtable;
-}
 
+    const uint8_t *tail = (const uint8_t *)(data + nblocks * 8);
+    uint64_t k1 = 0;
 
-void htable_insert_node(HTable_Node **hashtable, size_t length, HTable_Node *node, size_t (*hash)(const void *, size_t))
-{
-    size_t index = hash(node->data->key, length);
-    if(hashtable[index] != NULL)
-        node->next = hashtable[index];
-    hashtable[index] = node;
-}
-
-
-void htable_delete_node(HTable_Node **hashtable, size_t length, void *key, int (*cmp)(const void *, const void *), size_t (*hash)(const void *, size_t))
-{
-    size_t index = hash(key, length);
-    HTable_Node *tmp = hashtable[index], *previous = NULL;
-    while(tmp != NULL)
+    switch (len & 7)
     {
-        if(cmp(tmp->data->key, key) == 0)
+    case 7:
+        k1 ^= (uint64_t)tail[6] << 48;
+    case 6:
+        k1 ^= (uint64_t)tail[5] << 40;
+    case 5:
+        k1 ^= (uint64_t)tail[4] << 32;
+    case 4:
+        k1 ^= (uint64_t)tail[3] << 24;
+    case 3:
+        k1 ^= (uint64_t)tail[2] << 16;
+    case 2:
+        k1 ^= (uint64_t)tail[1] << 8;
+    case 1:
+        k1 ^= (uint64_t)tail[0];
+        k1 *= c1;
+        k1 = (k1 << 31) | (k1 >> 33);
+        k1 *= c2;
+        h1 ^= k1;
+    }
+
+    h1 ^= len;
+    h1 ^= h1 >> 33;
+    h1 *= 0xff51afd7ed558ccdULL;
+    h1 ^= h1 >> 33;
+    h1 *= 0xc4ceb9fe1a85ec53ULL;
+    h1 ^= h1 >> 33;
+
+    return h1;
+}
+
+static size_t hash(uint64_t key, size_t length)
+{
+    uint64_t hash_value = murmurhash3_64(&key, sizeof(key), 0x9747b28c);
+    return hash_value % length;
+}
+
+static void resize(htable_t *htable)
+{
+    size_t new_size = next_prime(htable->num_buckets * 1.5);
+    htable_node_t **new_buckets = calloc(new_size, sizeof(htable_node_t *));
+
+    for (size_t i = 0; i < htable->num_buckets; i++)
+    {
+        htable_node_t *node = htable->buckets[i];
+        while (node != nullptr)
         {
-            htable_deallocate_data(tmp->data);
-            if(previous != NULL)
-                previous->next = tmp->next;
-            else
-                hashtable[index] = tmp->next;
-            free(tmp);
+            htable_node_t *next = node->next;
+
+            size_t new_bucket = hash(node->key, new_size);
+            node->next = new_buckets[new_bucket];
+            new_buckets[new_bucket] = node;
+
+            node = next;
+        }
+    }
+
+    free(htable->buckets);
+
+    htable->buckets = new_buckets;
+    htable->num_buckets = new_size;
+}
+
+
+void htable_init(htable_t *htable)
+{
+    htable->num_buckets = 17;
+    htable->num_elements = 0;
+    htable->buckets = calloc(htable->num_buckets, sizeof(htable_node_t *));
+}
+
+void htable_free(htable_t *htable)
+{
+    for (size_t i = 0; i < htable->num_buckets; i++)
+    {
+        while (htable->buckets[i] != nullptr)
+        {
+            htable_node_t *tmp = htable->buckets[i]->next;
+            free(htable->buckets[i]);
+            htable->buckets[i] = tmp;
+        }
+    }
+    free(htable->buckets);
+    htable->num_elements = 0;
+    htable->num_buckets = 0;
+}
+
+void htable_insert(htable_t *htable, uint64_t key, int value)
+{
+    size_t bucket = hash(key, htable->num_buckets);
+    for (htable_node_t *tmp = htable->buckets[bucket]; tmp != nullptr; tmp = tmp->next)
+    {
+        if (key == tmp->key)
+        {
+            tmp->value = value;
             return;
         }
-        previous = tmp;
-        tmp = tmp->next;
     }
+
+    htable_node_t *node = node_alloc(key, value);
+
+    node->next = htable->buckets[bucket];
+    htable->buckets[bucket] = node;
+    htable->num_elements++;
+
+    if (htable->num_elements >= htable->num_buckets * 0.8)
+        resize(htable);
 }
 
-
-HTable_Data *htable_search_node(HTable_Node **hash_table, size_t length, void *key, int (*cmp)(const void *, const void *), size_t (*hash)(const void *, size_t))
+int htable_get(htable_t *htable, uint64_t key)
 {
-    size_t index = hash(key, length);
-    for(HTable_Node *tmp = hash_table[index]; tmp != NULL; tmp = tmp->next)
+    size_t bucket = hash(key, htable->num_buckets);
+    for (htable_node_t *tmp = htable->buckets[bucket]; tmp != nullptr; tmp = tmp->next)
     {
-        if(cmp(tmp->data->key, key) == 0)
-            return tmp->data;
+        if (key == tmp->key)
+            return tmp->value;
     }
-    return NULL;
+    return INT_MIN;
 }
 
-
-void htable_deallocate(HTable_Node **hashtable, size_t length)
+void htable_delete(htable_t *htable, uint64_t key)
 {
-    for(int i = 0; i < length; i++)
+    size_t bucket = hash(key, htable->num_buckets);
+    htable_node_t *prev = nullptr;
+    for (htable_node_t *tmp = htable->buckets[bucket]; tmp != nullptr; tmp = tmp->next)
     {
-        while(hashtable[i] != NULL)
+        if (key == tmp->key)
         {
-            HTable_Node *tmp = hashtable[i]->next;
-            htable_deallocate_data(hashtable[i]->data);
-            free(hashtable[i]);
-            hashtable[i] = tmp;
+            if(prev != nullptr)
+                prev->next = tmp->next;
+            else
+                htable->buckets[bucket] = tmp->next;
+            free(tmp);
+            htable->num_elements--;
+            return;
         }
+        prev = tmp;
     }
 }
--- hashtable/hashtable.h
+++ hashtable/hashtable.h
@@ -2,33 +2,26 @@
 #define HASHTABLE_H
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <wchar.h>
-
-typedef struct
-{
-    char *key;
-    char *value;
-} htable_data_t;
 
 typedef struct htable_node
 {
-    htable_data_t *data;
+    uint64_t key;
+    int value;
     struct htable_node *next;
 } htable_node_t;
 
+typedef struct
+{
+    size_t num_buckets;
+    size_t num_elements;
+    htable_node_t **buckets;
+} htable_t;
 
-int cmputf8(const void *wcs1, const void *wcs2);
-size_t wcshash(wchar_t *key, size_t length);
-size_t strhash(char *key, size_t length);
-HTable_Data *htable_create_data(char *key, char *value);
-void htable_deallocate_data(HTable_Data *data);
-HTable_Node *htable_create_node(HTable_Data *data);
-HTable_Node **htable_create(size_t length);
-void htable_insert_node(HTable_Node **hashtable, size_t length, HTable_Node *node, size_t (*hash)(const void *, size_t));
-void htable_delete_node(HTable_Node **hashtable, size_t length, void *key, int (*cmp)(const void *, const void *), size_t (*hash)(const void *, size_t));
-HTable_Data *htable_search_node(HTable_Node **hash_table, size_t length, void *key, int (*cmp)(const void *, const void *), size_t (*hash)(const void *, size_t));
-void htable_deallocate(HTable_Node **hash_table, size_t length);
+void htable_delete(htable_t *htable, uint64_t key);
+void htable_free(htable_t *htable);
+void htable_init(htable_t* htable);
+void htable_insert(htable_t *htable, uint64_t key, int value);
+int htable_get(htable_t *htable, uint64_t key);
+void htable_delete(htable_t *htable, uint64_t key);
 
 #endif
