d71f448d4ecdc96886a2337427ca6583a2a162fd
Felipe Fraxe <felipe_fraxe@hotmail.com>
2025-06-29 02:34:18 -0400

    Refactor searching algorithms in arrays

---
+++ search/binary_search/binsearch.c
@@ -0,0 +1,19 @@
+/*
+  Code written by Felipe Fraxe Filho
+  Just for learning
+*/
+
+int binsearch(int array[], int lo, int hi, int target)
+{
+    while(lo <= hi)
+    {
+        int mid = lo + (hi - lo) / 2;
+        if (array[mid] == target)
+            return mid;
+        if (array[mid] < target)
+            lo = mid + 1;
+        else
+            hi = mid - 1;
+  }
+  return -1;
+}
--- search/binary_search/quick_sort/quick.h
+++
@@ -1,9 +0,0 @@
-#ifndef QUICK_H
-#define QUICK_H
-
-// Quick Sort prototypes
-void quick_sort(int *array, int left, int right);
-int partition(int *array, int left, int right, int pivot);
-void swap(int *elem1, int *elem2);
-
-#endif
