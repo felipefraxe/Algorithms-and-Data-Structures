bcc86617f03e26d177d2e626b4f30c6ba251cae9
Felipe Fraxe <felipe_fraxe@hotmail.com>
2026-06-01 14:08:50 -0400

    refactor: Improved mege sort for readability and one allocation for aux array

--- sort/merge_sort/merge.c
+++ sort/merge_sort/merge.c
@@ -3,89 +3,44 @@
   Just for learning
 */
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 
 #include "merge.h"
 
-// Define Array's size
-#define SIZE 2000000
-
-void swap(int *elem1, int *elem2);
-void generate_array(int *array, int size);
-void print_array(int *array, int size);
-
-int main(void)
+void merge_sort(int array[], int size)
 {
-    srand(time(0));
-    int array[SIZE];
-    generate_array(array, SIZE);
-    
-    clock_t t = clock();
-    merge_sort(array, 0, SIZE - 1);
-    t = clock() - t;
-    
-    print_array(array, SIZE);
-    printf("This method took %lf seconds to sort\n", (double) t / CLOCKS_PER_SEC);
-    return EXIT_SUCCESS;
+    int *aux = malloc(sizeof(int) * size);
+    msort(array, aux, 0, size - 1);
+    free(aux);
 }
 
-void merge_sort(int *array, int low, int high)
+void msort(int array[], int aux[], int lo, int hi)
 {
-    if (low >= high)
+    if (lo >= hi)
         return;
-    
-    int mid = low + (high - low) / 2;
-    merge_sort(array, low, mid);
-    merge_sort(array, mid + 1, high);
-    merge(array, low, mid, high);
+
+    int mid = lo + ((hi - lo) >> 1);
+    msort(array, aux, lo, mid);
+    msort(array, aux, mid + 1, hi);
+    merge(array, aux, lo, mid, hi);
 }
 
-void merge(int *array, int low, int mid, int high)
+void merge(int array[], int aux[], int lo, int mid, int hi)
 {
-    int size1 = (mid + 1) - low;
-    int size2 = high - mid;
-    int array1[size1], array2[size2];
+    for (int i = lo; i <= hi; i++)
+        aux[i] = array[i];
 
-    for(int i = 0, k = low; i < size1; i++, k++)
-        array1[i] = array[k];
-
-    for(int j = 0, k = mid + 1; j < size2; j++, k++)
-        array2[j] = array[k];
-
-    int i = 0, j = 0, k = low;
-    while(i < size1 && j < size2)
+    int i = lo;
+    int j = mid + 1;
+    int k = lo;
+    while (i <= mid && j <= hi)
     {
-        if(array1[i] < array2[j])
-            array[k++] = array1[i++];
+        if (aux[i] <= aux[j])
+            array[k++] = aux[i++];
         else
-            array[k++] = array2[j++];
+            array[k++] = aux[j++];
     }
 
-    while(i < size1)
-        array[k++] = array1[i++];
-    while(j < size2)
-        array[k++] = array2[j++];
-}
-
-void swap(int *elem1, int *elem2)
-{
-    int tmp = *elem1;
-    *elem1 = *elem2;
-    *elem2 = tmp;
-}
-
-void generate_array(int *array, int size)
-{
-    for(int i = 0; i < size; i++)
-        array[i] = rand() / (RAND_MAX / size + 1);
-}
-
-void print_array(int *array, int size)
-{
-    for(int i = 0; i < size; i++)
-        printf("%d ", array[i]);
-    printf("\n");
+    while (i <= mid)
+        array[k++] = aux[i++];
 }
--- sort/merge_sort/merge.h
+++ sort/merge_sort/merge.h
@@ -2,7 +2,8 @@
 #define MERGE_H
 
 // Prototypes for Merge Sort
-void merge_sort(int *array, int low, int high);
-void merge(int *array, int low, int mid, int high);
+void merge_sort(int array[], int size);
+void msort(int array[], int aux[], int lo, int hi);
+void merge(int array[], int aux[], int lo, int mid, int hi);
 
 #endif
